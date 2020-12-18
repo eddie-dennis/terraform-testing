55d637360a9b7d56d26d0fa7c9bbeb55aea853bf
Danny Smith <dannyleesmith@users.noreply.github.com>
2020-12-18 09:58:03 +0000

    Fix errors and syntax

--- main.tf
+++ main.tf
@@ -16,11 +16,14 @@ terraform {
 
 provider "gcp" {
   region = "us-central1"
+}
   
 resource "google_compute_instance" "default" {
   name         = "vm-web-test"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
+  
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Goodbye World!</h1></body></html>' | sudo tee /var/www/html/index.html"
 
   boot_disk {
     initialize_params {
@@ -31,27 +34,23 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
 
-    access_config {
-    }
+    access_config {}
   }
 
-    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Goodbye World!</h1></body></html>' | sudo tee /var/www/html/index.html"
-
-    tags = ["http-server"]
- }
+  tags = ["http-server"]
 }
   
-resource "google_compute_firewall" "http-server" {
+resource "google_compute_firewall" "http_server" {
   name    = "default-allow-http-terraform"
   network = "default"
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
 
   allow {
     protocol = "tcp"
     ports    = ["80"]
   }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
 }
 
 output "ip" {
