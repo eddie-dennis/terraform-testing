e757696c2877a5ca0a4833835b3462bb78a64db9
eddie-dennis <31904027+eddie-dennis@users.noreply.github.com>
2020-12-18 00:03:17 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,8 +43,9 @@ resource "google_compute_instance" "default" {
     metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Goodbye World!</h1></body></html>' | sudo tee /var/www/html/index.html"
 
     tags = ["http-server"]
+ }
 }
-
+  
 resource "google_compute_firewall" "http-server" {
   name    = "default-allow-http-terraform"
   network = "default"
